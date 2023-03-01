0bb3bdfc47c091da8626b5584b316d3e4e3e563c
Paula <paulactalim@gmail.com>
2023-03-01 00:41:35 -0300

    criando funcao para fazer a transcricao do arq csv para db

--- src/App.java
+++ src/App.java
@@ -27,30 +27,27 @@ public class App {
         return sb.toString();
     }
 
-    public static void main(String[] args) {
-        registro r;
+    public static void passar_arq_csv_para_db (RandomAccessFile arq2)  {
+        String linha;
         int id_metadados = 0;
         String[] vet;
         byte[] registro_vet_byte;
+        registro r;
 
-        //Declara o arquivo db
-		FileOutputStream arq;
+        FileOutputStream arq_db;
 	    DataOutputStream dos;
-        RandomAccessFile arq2;
 
-        //Declara o arquivo csv
-        File arq_csv = new File("src\\planilha_teste.csv");        
+        File arq_csv = new File("src\\planilha_teste.csv");
         Scanner scanner = null;
-        String linha;
 
         try {
             //Abre os objetos do arquivos db
-			arq = new FileOutputStream("src/base.db");
-	        dos = new DataOutputStream(arq);
+            arq_db = new FileOutputStream("src/base.db");
+            dos = new DataOutputStream(arq_db);
 
             //Le arquivos csv
             scanner = new Scanner (arq_csv);
-            
+
             //Le o cabecalho da planilha
             linha = new String(scanner.nextLine());
 
@@ -63,10 +60,10 @@ public class App {
                 
                 //Trata a string lida e a coloca em um vetor de string
                 vet = tratar_string(linha).split(",");                
-    
+
                 //Cria o regirtro
                 r = new registro (id_metadados, vet[0], Integer.parseInt(vet[1]), vet[2].charAt(0));
-    
+
                 //Escreve o registro
                 registro_vet_byte = r.toByteArray();
                 dos.writeByte(' ');
@@ -77,27 +74,98 @@ public class App {
                 id_metadados++;
             }
 
-            //Atualiza os metadados
-            arq.close();
+            arq_db.close();
             dos.close();
+
             arq2 = new RandomAccessFile("src/base.db", "rw");
             
-           
             arq2.writeInt(id_metadados);
-
             arq2.close();
+
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        } finally {
+            scanner.close();            
+        }
+    }
+
+    public static void main(String[] args) {
+        registro r;
+        int id_metadados = 0;
+        String[] vet;
+        byte[] registro_vet_byte;
+
+        //Declara o arquivo db
+		FileOutputStream arq;
+	    DataOutputStream dos;
+        RandomAccessFile arq2;
+
+        //passar_arq_csv_para_db(arq2);
+
+        try {
+            arq2 = new RandomAccessFile("src/base.db", "rw");
+            passar_arq_csv_para_db(arq2);
+        //Declara o arquivo csv
+        // File arq_csv = new File("src\\planilha_teste.csv");        
+        // Scanner scanner = null;
+        // String linha;
+
+        // try {
+        //     //Abre os objetos do arquivos db
+		// 	arq = new FileOutputStream("src/base.db");
+	    //     dos = new DataOutputStream(arq);
+
+        //     //Le arquivos csv
+        //     scanner = new Scanner (arq_csv);
+            
+        //     //Le o cabecalho da planilha
+        //     linha = new String(scanner.nextLine());
+
+        //     //Escreve os metadados no arquivo
+        //     dos.writeInt(id_metadados);
+
+        //     //Le o arquivo csv e passa as informacoes para o arquivo db
+        //     while (scanner.hasNextLine()) {
+        //         linha = new String(scanner.nextLine());
+                
+        //         //Trata a string lida e a coloca em um vetor de string
+        //         vet = tratar_string(linha).split(",");                
+    
+        //         //Cria o regirtro
+        //         r = new registro (id_metadados, vet[0], Integer.parseInt(vet[1]), vet[2].charAt(0));
+    
+        //         //Escreve o registro
+        //         registro_vet_byte = r.toByteArray();
+        //         dos.writeByte(' ');
+        //         dos.writeInt(registro_vet_byte.length);
+        //         dos.write(registro_vet_byte);
+
+        //         //Atualiza o id
+        //         id_metadados++;
+        //     }
+
+        //     //Atualiza os metadados
+        //     arq.close();
+        //     dos.close();
+        //     arq2 = new RandomAccessFile("src/base.db", "rw");
+            
+           
+        //     arq2.writeInt(id_metadados);
+
+           //arq2.close();
 
             
 
         } catch (NumberFormatException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
-    
-        } finally {
-            scanner.close();            
         }
+        // } finally {
+        //     scanner.close();            
+        // }
     }
 }
