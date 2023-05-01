f590a89de521cb77c7801f0aa1e1b635dc889a57
Paula <paulactalim@gmail.com>
2023-05-01 18:16:25 -0300

    evoluindo hash

--- src/trabalho_2/indexacao.java
+++ src/trabalho_2/indexacao.java
@@ -12,6 +12,82 @@ public class indexacao {
         return chave % (int) Math.pow(2, profundidade);
     }
 
+    public static long pesquisa_buckets (int id_bucket) {
+        return id_bucket * 124;
+    }
+
+    public static long pesquisa_diretorio (int id_chave) {
+        return 2 + (id_chave * 8);
+    }
+
+    public static void aumentar_profundidade_diretorio (RandomAccessFile arq_buckets, RandomAccessFile arq_diretorio, short profundidade_diretorio) throws Exception {
+        arq_diretorio.seek(arq_diretorio.length());
+
+        int qnt_buckets_cria;
+
+        //Calcula a quantidade de buckets a ser criado
+        if (profundidade_diretorio != 1) {
+            qnt_buckets_cria = (int) Math.pow(2, profundidade_diretorio) / 2;
+
+            for (int i = 0; i < qnt_buckets_cria; i++) {
+                arq_diretorio.writeLong(pesquisa_buckets(i));
+            }
+        }
+    }
+
+    public static void utilizar_novo_bucket (RandomAccessFile bucket, RandomAccessFile diretorio, int id_bucket_dividir) throws Exception{
+        bucket.seek(pesquisa_diretorio(id_bucket_dividir));
+
+        diretorio.seek(0);
+
+        short profundidade_diretorio = diretorio.readShort();
+        short profundidade_bucket = bucket.readShort();
+        profundidade_bucket ++;
+
+        if (profundidade_diretorio == profundidade_bucket) {
+            profundidade_diretorio ++;
+
+            diretorio.seek(0);
+            diretorio.writeShort(profundidade_diretorio);
+            aumentar_profundidade_diretorio(bucket, diretorio, profundidade_diretorio);
+        }
+
+        //Atualiza o diretorio
+        //diretorio.seek(pesquisa_diretorio(profundidade_bucket));
+
+        //Atualiza profundidade e tamanho do bucket
+        bucket.writeShort(profundidade_bucket);
+        bucket.writeInt(0);
+
+        int[] id_registro = new int[10];
+        long[] endereco_registro = new long[10];
+
+        //Le os reistros no bucket
+        for (int i = 0; i < 10; i++) {
+            id_registro[i] = bucket.readInt();
+            endereco_registro[i] = bucket.readLong();
+        }
+
+        //redividindo registros
+        for (int i = 0; i < 10; i++) {
+            int hash = funcao_hash(id_registro[i], profundidade_bucket);
+            
+            bucket.seek(pesquisa_buckets(hash) + 2);
+
+            int tamanho = bucket.readInt();
+
+            //Colocando registro
+            bucket.seek(tamanho*12);
+            bucket.writeInt(id_registro[i]);
+            bucket.writeLong(endereco_registro[i]);
+            
+            //Atualiza o tamanho
+            tamanho++;
+            bucket.seek(tamanho - 4);
+            bucket.writeInt(tamanho);
+        }
+    }
+
     public static void teste () throws Exception {
         String diretorio_indices = "src/arquivos_de_indices";
 
@@ -24,10 +100,12 @@ public class indexacao {
         RandomAccessFile buckets = new RandomAccessFile(diretorio_indices + "/buckets.db", "rw");
         RandomAccessFile data_base = new RandomAccessFile("src/pokedex.db", "rw");
 
-        //Profundidade
-        diretorio.writeShort(0);
+        short profundidade_diretorio = 1;
 
-        diretorio.writeInt(1);
+        //Profundidade
+        diretorio.writeShort(profundidade_diretorio);
+
+        diretorio.writeInt(0);
 
         data_base.readInt();
 
--- test/funcao_hash/src/App.java
+++ test/funcao_hash/src/App.java
@@ -6,6 +6,17 @@ public class App {
         int result1 = (int) Math.pow(2, profundidade);
         int result = chave % (int) Math.pow(2, profundidade);
 
-        System.out.println(result);
+        int qnt_buckets_cria;
+
+        int profundidade_diretorio = 4;
+
+        if (profundidade_diretorio != 1) {
+            qnt_buckets_cria = (int) Math.pow(2, profundidade_diretorio) / 2;
+        } else {
+            qnt_buckets_cria = 2;
+        }
+
+
+        System.out.println(qnt_buckets_cria);
     }
 }
